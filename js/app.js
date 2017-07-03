266ce2cd3a65068ec1cb3ac6e03357173b3ded1c
Vladimir <dosandkv@gmail.com>
2017-07-04 00:54:58 +0300

    Added events for: like/dislike movie;

--- js/app.js
+++ js/app.js
@@ -1,49 +1,126 @@
-const downloadBtnEl = document.getElementById('download-movies');
-const clearBtnEl = document.getElementById('clear-movies');
-const contentEl = document.querySelector('.carousel-items-list');
+const app = {
+  initialize() {
+    this.dataDownloaded = false;
 
-downloadBtnEl.addEventListener('click', event => {
-  console.error('click', event);
+    this.cacheElements();
+    this.fireListeners();
+  },
+  cacheElements() {
+    this.elements = {};
 
-  const DOMAIN_URL = 'http://localhost:3000';
-  const api = `${DOMAIN_URL}/api`;
-  const externalApi = 'https://api.themoviedb.org/3/movie/popular?api_key=59ff214635b431c1656379bf5aa01a8a&language=en-US&page=1';
+    this.elements.downloadBtn = document.getElementById('download-movies');
+    this.elements.clearBtn = document.getElementById('clear-movies');
+    this.elements.content = document.querySelector('.carousel-items-list');
+    this.elements.status = document.querySelector('.connection-status');
+    this.elements.rateMovie = document.querySelectorAll('.js-rate-movie');
+    this.elements.moviesList = document.querySelector('.movies-list');
+  },
+  fireListeners() {
+    const listeners = {
+      'window:online': 'toggleStatus',
+      'window:offline': 'toggleStatus',
+      'document:DOMContentLoaded': 'insertStatus',
+      'clearBtn:click': 'clearImages',
+      'downloadBtn:click': 'downloadMovies',
+      'rateMovie:click': 'rateMovie',
+    };
 
-  const initObj = {
-    method: 'GET',
-    headers: new Headers({
-      'Content-Type': 'application/json'
+    Object.keys(listeners).forEach(key => {
+      const [elementName, eventType] = key.split(':');
+      const value = listeners[key];
+      const element = this.elements[elementName] || window[elementName];
+      const addListener = item => item.addEventListener(eventType, this[value].bind(this));
+
+      if (element.length) {
+        this.elements[elementName].forEach(item => {
+          addListener(item)
+        })
+      } else {
+        addListener(element);
+      }
     })
-  };
+  },
+  rateMovie(e) {
+    const img = document.querySelector('.carousel-item.active img');
+    const {rate} = e.target.dataset;
 
-  fetch(externalApi, initObj)
-    .then(data => {
-      data.json().then(response => showImages(response))
-    })
-    .catch(err => console.error(err));
-});
+    if (img) {
+      const id = parseInt(img.getAttribute('alt'));
+      const movie = this.movies.find(item => item.id === id);
 
-clearBtnEl.addEventListener('click', () => {
-  while (contentEl.firstChild) {
-    contentEl.removeChild(contentEl.firstChild);
-  }
-});
+      const li = document.createElement('li');
 
-function showImages (data) {
-  data.results.forEach((item, index) => {
-    const div = document.createElement('div');
-    const img = document.createElement('img');
+      li.textContent = `${rate}: ${movie.title}`;
 
-    div.classList.add('carousel-item');
+      this.elements.moviesList.appendChild(li);
+    }
+  },
+  toggleStatus(e) {
+    const {status} = this.elements;
+    const {type} = e;
 
-    if (index === 0) {
-      div.classList.add('active');
+    status.classList.toggle('online');
+    status.textContent = type;
+  },
+  insertStatus() {
+    const {status} = this.elements;
+    const type = navigator.onLine ? 'online' : 'offline';
+
+    status.classList.toggle(type);
+    status.textContent = type;
+  },
+  clearImages() {
+    const {content} = this.elements;
+
+    while (content.firstChild) {
+      content.removeChild(content.firstChild);
     }
 
-    img.src = `https://image.tmdb.org/t/p/w300${item.poster_path}`;
+    this.dataDownloaded = false;
+  },
+  showImages(data) {
+    data.results.forEach((item, index) => {
+      const div = document.createElement('div');
+      const img = document.createElement('img');
 
-    div.appendChild(img);
+      div.classList.add('carousel-item');
 
-    contentEl.appendChild(div);
-  });
-}
+      if (index === 0) {
+        div.classList.add('active');
+      }
+
+      img.src = `https://image.tmdb.org/t/p/w300${item.poster_path}`;
+      img.alt = item.id;
+
+      div.appendChild(img);
+
+      this.elements.content.appendChild(div);
+    });
+  },
+  downloadMovies() {
+    const DOMAIN_URL = 'http://localhost:3000';
+    const api = `${DOMAIN_URL}/api`;
+    const externalApi = 'https://api.themoviedb.org/3/movie/popular?api_key=59ff214635b431c1656379bf5aa01a8a&language=en-US&page=1';
+
+    const initObj = {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    if (!this.dataDownloaded) {
+      fetch(externalApi, initObj)
+      .then(data => {
+        data.json().then(response => {
+          this.dataDownloaded = true;
+          this.movies = response.results;
+          this.showImages(response);
+        })
+      })
+      .catch(err => console.error(err));
+    }
+  }
+};
+
+app.initialize();
