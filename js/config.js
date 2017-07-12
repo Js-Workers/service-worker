d0e69d82773ab5cab8959d8a9a1417c6e8b5e3dc
Vladimir <dosandkv@gmail.com>
2017-07-13 01:22:18 +0300

    Finished v0.0.1;

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const config = {
+  MLAB_API_KEY: 'p2TP7XLURQhMSf4Nv3BGCIIMwk5D5s3h',
+  TMDB_API_KEY: '59ff214635b431c1656379bf5aa01a8a',
+  IMG_POSTER_DOMAIN: 'https://image.tmdb.org/t/p/w300',
+  MOVIE_DOMAIN: 'https://www.themoviedb.org/movie/',
+  MOVIE_API: 'https://api.themoviedb.org/3/movie/'
+};
+
