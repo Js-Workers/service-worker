89739e3bfc7c891a3af7484b70bf642210fd3546
Volodymyr Shevchuk <dosandkv@gmail.com>
2017-07-20 14:16:08 +0300

    Added example for WW;

--- examples/web-worker/web-worker.js
+++ examples/web-worker/web-worker.js
@@ -1,5 +1,18 @@
 self.addEventListener('message', event => {
+  const {data} = event;
+  const fib = n => {
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  };
+
   setTimeout(() => {
-    postMessage(event.data);
+    switch (data.type) {
+      case 'fib':
+        const result = fib(data.body);
+
+        postMessage({result});
+        break;
+      default:
+        postMessage(event.data);
+    }
   }, 2000);
 });
